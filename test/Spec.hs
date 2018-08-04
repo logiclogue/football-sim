8000c2eb423443179cbef019bf9305a9e311f21a
logiclogue <jordanpaullord@hotmail.co.uk>
2018-08-04 15:50:52 +0100

    Adds working tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,7 @@
-import Test
+import Test.HUnit
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = assertEqual "True is true" True True
+
+anotherTest :: IO ()
+anotherTest = assertEqual "42 is 42" 42 41
