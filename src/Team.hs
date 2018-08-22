e0b57e688fe42d665b713808e81f4abff1b81720
logiclogue <jordanpaullord@hotmail.co.uk>
2018-08-22 22:44:25 +0100

    Adding tests for team

--- src/Team.hs
+++ src/Team.hs
@@ -1,3 +1,9 @@
-module Team (Team) where
+module Team (Team, name, rating) where
 
 newtype Team = Team String Integer
+
+name :: Team -> String
+name (Team x _) = x
+
+rating :: Team -> Integer
+rating (Team _ x) = x
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,17 @@
 import Test.HUnit
 import Lib
+import Team
 
 main :: IO Counts
 main = runTestTT tests
 
 tests :: Test
 tests = TestList [
+    TestLabel
+        "Team"
+        (TestList [
+            
+        ])
     TestLabel
         "test1"
         (TestCase (assertEqual "42 is 42" 42 (add42 0))),
